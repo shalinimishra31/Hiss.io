2f9fe5a26f2b4bba256ba231a83d2e843a1a7d80
shalinimishra31 <79595208+shalinimishra31@users.noreply.github.com>
2022-01-28 22:36:24 +0530

    Update index3.js

--- index3.js
+++ index3.js
@@ -78,7 +78,7 @@ if(snake.maxCells<23)
   }
 
   // draw apple
-  context.fillStyle = 'yellow';
+  context.fillStyle = '#ff006e';
   context.fillRect(apple.x, apple.y, grid-1, grid-1);
 
   // draw snake one cell at a time
